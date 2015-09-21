4cf395a3d2385e7f27fbbb88309987c1be601046
jiang-w <frank@imac-bigdata.local>
2015-09-21 11:36:48 +0800

    commit

--- Pods/Target Support Files/Pods-GoldenSignal-KVOController/Pods-GoldenSignal-KVOController-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GoldenSignal_KVOController : NSObject
-@end
-@implementation PodsDummy_Pods_GoldenSignal_KVOController
-@end
--- Pods/Target Support Files/Pods-GoldenSignal-PPiAwesomeButton/Pods-GoldenSignal-PPiAwesomeButton-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GoldenSignal_PPiAwesomeButton : NSObject
-@end
-@implementation PodsDummy_Pods_GoldenSignal_PPiAwesomeButton
-@end
