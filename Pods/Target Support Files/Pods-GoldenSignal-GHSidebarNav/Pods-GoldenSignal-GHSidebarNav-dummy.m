a92f0d92052ccbb72197a95c73ce2b4f4bea86a7
jiang-w <frank@imac-bigdata.local>
2015-09-21 13:59:02 +0800

    commit

--- Pods/Target Support Files/Pods-GoldenSignal-GHSidebarNav/Pods-GoldenSignal-GHSidebarNav-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GoldenSignal_GHSidebarNav : NSObject
-@end
-@implementation PodsDummy_Pods_GoldenSignal_GHSidebarNav
-@end
--- Pods/Target Support Files/Pods-GoldenSignal-Masonry/Pods-GoldenSignal-Masonry-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GoldenSignal_Masonry : NSObject
-@end
-@implementation PodsDummy_Pods_GoldenSignal_Masonry
-@end
